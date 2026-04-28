d2abdb4b84a500b41d30dee847c71624b788b6f7
ky <ky>
2026-04-28 23:13:21 +0900

    update setting version

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     //=========== public properties =========== 
-    objOpts.information.version = "2.3";
+    objOpts.information.version = "2.4";
     objOpts.developer.mode = "user";//"user";"developer";"root"; 
     objOpts.canvas.use_touchpad_mode = true;
     objOpts.canvas.depth_scale = "composite_depth";
